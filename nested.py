e6e2ea6a0505b85cc65121ec561bb20c510fa0a9
Janell Huyck <Janell.Huyck@outlook.com>
2020-02-11 22:56:58 -0500

    refactored code so that it reads from string directly and splices that rather than creating a formatted list

--- nested.py
+++ nested.py
@@ -19,100 +19,38 @@ import sys
 if sys.version_info[0] >= 3:
     raise Exception("This program requires python2 interpreter")
 
-# read input line by line from input.txt
-#   on each line, run the tests and then write/append to output.txt
-
-# Types of brackets:
-#     ()
-#     []
-#     {}
-#     < >
-#     (* *)
-# The combination `(*)` should be interpreted as `(*` followed by `)`.
-# `(*` and `*)`count as one for the error space
-
-
-# change the input string into a list of single characters
-# need to include whitespace.
-
-# iterate through that list and merge the (* and *) elements
-
-# make a "stack" list of all brackets, going left to right in the string
-
-# if a bracket is a left symbol, add it to the list.
-
-# if a bracket is a right symbol, compare it to the last item in the list.
-# it must be the complement.  If it is, delete the bracket from the list.
-# if it is not the complement, we have an error.  proceed to finding the
-# location of the error.
-
-# finding error location:
-# make a counter for the merged (* array to show what character we are
-# currently evaluating
-
-# write output to output.txt - line by line
-#
-# The output is a textfile named `output.txt`.
-# Each line contains the result of the check of the corresponding
-# inputline, that is ‘YES’ (in upper case), if the expression is OK,
-# and (if it is not OK) ‘NO’ followed by a space and the
-# position of the error.
-
 
 def is_nested(line):
-    line = make_formatted_line(line)
-    left_parenthesis_values = ["(", "[", "{", "<", "(*"]
-    right_parenthesis_values = [")", "]", "}", ">", "*)"]
+    l_paren_vals = ["(*", "(", "[", "{", "<"]
+    r_paren_vals = ["*)", ")", "]", "}", ">"]
     left_stack = []
+    index = 0
 
-    for character_index in range(len(line)):
-        character = line[character_index]
-        error_index = character_index + 1
-        if character in left_parenthesis_values:
-            left_stack += [character]
-        elif character in right_parenthesis_values:
-            if len(left_stack) > 0:
-                last_left = left_stack[-1]
-                if left_parenthesis_values.index(last_left) ==\
-                        right_parenthesis_values.index(character):
+    while line:
+        token = line[0]
+        index += 1
+        for paren in l_paren_vals:
+            if line.startswith(paren):
+                token = paren
+                left_stack.append(token)
+                break
+        for paren in r_paren_vals:
+            if line.startswith(paren):
+                token = paren
+                if len(left_stack) == 0:
+                    return "NO " + str(index)
+                if r_paren_vals.index(paren) ==\
+                        l_paren_vals.index(left_stack[-1]):
                     left_stack.pop()
+                    break
                 else:
-                    return "NO " + str(error_index)
-            else:
-                return "NO " + str(error_index)
-    if len(left_stack) != 0:
-        return "NO " + str(error_index)
-    else:
+                    return "NO " + str(index)
+        line = line[len(token):]
+
+    if len(left_stack) == 0:
         return "YES"
-
-
-def make_formatted_line(line):
-    line = [character for character in line]
-    formatted_line = []
-    skip = False
-    for character_index in range(0, len(line)):
-        if skip is True:
-            skip = False
-            continue
-        if character_index == len(line)-1:
-            formatted_line += [(line[character_index])]
-        elif line[character_index] != "(" and line[character_index] != "*":
-            formatted_line += [(line[character_index])]
-        elif line[character_index] == "(" and line[character_index + 1] == "*":
-            skip = True
-            formatted_line += [("(*")]
-        elif line[character_index] == "(" and line[character_index + 1] != "*":
-            formatted_line += [(line[character_index])]
-        elif line[character_index] == "*" and line[character_index + 1] == ")":
-            skip = True
-            formatted_line += [("*)")]
-        elif line[character_index] == "*" and line[character_index + 1] != ")":
-            formatted_line += [(line[character_index])]
-        else:
-            print ("I goofed up somewhere in the parsing of the line.")
-    return formatted_line
-
-    print (line)
+    else:
+        return "NO " + str(index)
 
 
 def main(args):
